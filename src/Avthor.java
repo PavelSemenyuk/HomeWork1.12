ba5f9c76496181010736cd8ecc7288b290cf3dba
PavelSemenyuk <pavel.semenuk.spb@gmail.com>
2023-01-30 22:36:40 +0300

    Start Commit

---
+++ src/Avthor.java
@@ -0,0 +1,16 @@
+public class Avthor {
+    private String nameAvtor;
+    private String surname;
+
+    public Avthor(String nameAvtor, String surname) {
+        this.nameAvtor = nameAvtor;
+        this.surname = surname;
+
+    }
+    public String getNameAvtor(){
+        return this.nameAvtor;
+    }
+    public String getSurname(){
+        return this.surname;
+    }
+}
--- src/HomeWork.java
+++ src/HomeWork.java
@@ -1,5 +1,24 @@
 public class HomeWork {
     public static void main(String[] args) {
+        Book masterM =  new Book("Мастер и Маргорита", 1928);
+        System.out.println("Название книги = " + masterM.getName());
+        System.out.println("Год публикации = " + masterM.getDate());
+        masterM.setDate(1956);
+        System.out.println("Год переиздания = " + masterM.getDate());
+
+        Avthor bylgakov = new Avthor("Михаил ", "Булгаков");
+        System.out.println("Имя автора = " + bylgakov.getNameAvtor() + bylgakov.getSurname());
+
+        System.out.println();
+
+        Book atlas = new Book("Атлант расправил плечи", 1957);
+        System.out.println("Название книги = " + atlas.getName());
+        System.out.println("Год публикации = " + atlas.getDate());
+        atlas.setDate(1988);
+        System.out.println("Год переиздания = " + atlas.getDate());
+
+        Avthor aynRand = new Avthor("Айн ","Рэнд");
+        System.out.println("Имя автора = " + aynRand.getNameAvtor() + aynRand.getSurname());
 
     }
 }
